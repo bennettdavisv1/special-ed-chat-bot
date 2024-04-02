7e34631797d0525188ccde2ffdfa4a574c1eeb6c
BenDavis36 <bennett0730@gmail.com>
2024-04-02 18:37:47 -0500

    Hi

--- Frontend/JS/general.js
+++ Frontend/JS/general.js
@@ -1,27 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('title').addEventListener('click', function() {
-      window.location.href = 'index.html';
-    });
+  document.getElementById('title').addEventListener('click', function () {
+    window.location.href = 'index.html';
   });
-  
-  document.getElementById('aboutButton').addEventListener('click', function() {
-    window.location.href = 'aboutPage.html';
-  });
-  
-  function resizeTextarea(id) {
-    const textarea = document.getElementById(id);
-    textarea.style.height = '';
-    textarea.style.height = Math.min(textarea.scrollHeight, 300) + 'px';
-  }
-  
-  document.getElementById('userInput')?.addEventListener('input', function() {
-    resizeTextarea('userInput');
+});
+
+document.getElementById('aboutButton').addEventListener('click', function () {
+  window.location.href = 'aboutPage.html';
+});
+
+function resizeTextarea(id) {
+  const textarea = document.getElementById(id);
+  textarea.style.height = '';
+  textarea.style.height = Math.min(textarea.scrollHeight, 300) + 'px';
+}
+
+document.getElementById('userInput')?.addEventListener('input', function () {
+  resizeTextarea('userInput');
 });
 
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
+
+function toggleMenu() {
+  const navMenu = document.getElementById('navMenu');
+  if (navMenu.classList.contains('hidden')) {
+    navMenu.classList.remove('hidden');
+  } else {
+    navMenu.classList.add('hidden');
+  }
 }
